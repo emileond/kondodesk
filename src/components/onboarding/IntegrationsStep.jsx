37b0266e38c88b9478037be7e4267f0ef7524e89
Emilio Sanchez <sonarart@gmail.com>
2025-07-17 23:20:24 -0600

    onboarding integrations step update

--- src/components/onboarding/IntegrationsStep.jsx
+++ src/components/onboarding/IntegrationsStep.jsx
@@ -3,18 +3,18 @@ import GithubIntegrationCard from '../integrations/github/GithubIntegrationCard.
 import ClickupIntegrationCard from '../integrations/clickup/ClickupIntegrationCard.jsx';
 import TrelloIntegrationCard from '../integrations/trello/TrelloIntegrationCard.jsx';
 import TodoistIntegrationCard from '../integrations/todoist/TodoistIntegrationCard.jsx';
-import TickTickIntegrationCard from '../integrations/ticktick/TickTickIntegrationCard.jsx';
 import { Button } from '@heroui/react';
+import AsanaIntegrationCard from '../integrations/asana/AsanaIntegrationCard.jsx';
 
 function IntegrationsStep({ goToNextStep }) {
     return (
         <div className="flex flex-col gap-3">
+            <AsanaIntegrationCard isCompact />
             <JiraIntegrationCard isCompact />
             <GithubIntegrationCard isCompact />
             <ClickupIntegrationCard isCompact />
             <TrelloIntegrationCard isCompact />
             <TodoistIntegrationCard isCompact />
-            <TickTickIntegrationCard isCompact />
             <Button color="primary" onPress={goToNextStep}>
                 Continue
             </Button>
